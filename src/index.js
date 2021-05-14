b7283650b5c5c9049c3ced3631a4d66943532a26
Tatyana Rezunenko <tanya.kamyshnikova@gmail.com>
2021-05-14 07:01:02 +0300

    slider

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 'use strict';
 import openModalStart from './module/openModal';
 import smoothScroll from './module/smoothScroll';
+import slider from './module/slider';
 
 openModalStart();
 smoothScroll();
+slider();
--- src/module/smoothScroll.js
+++ src/module/smoothScroll.js
@@ -1,27 +1,54 @@
 // eslint-disable-next-line strict
 'use strict';
 const smoothScroll = () => {
-  const scrollBlocks = document.querySelectorAll('a[href]');
+  const scrollUp = document.querySelector('.top-slider');
+  const up = document.querySelector('.up');
+  up.style.display = 'none';
 
-  for (const scrollBlock of scrollBlocks) {
-
-    scrollBlock.addEventListener('click', event => {
-      event.preventDefault();
-
-      const id = scrollBlock.getAttribute("href");
-
-      if (id !== '#') {
-        const getId = document.querySelector(id);
-
-        if (getId !== null) {
-          getId.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start',
-          });
-        }
-      }
+  const click = () => {
+    up.addEventListener('click', () => {
+      scrollUp.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+      });
     });
-  }
+  };
+  click();
+
+  window.addEventListener('scroll', () => {
+    if (pageYOffset >= 612) {
+      up.style.display = 'block';
+    } else {
+      up.style.display = 'none';
+    }
+  });
+
+
+  const scroll = () => {
+    const scrollBlocks = document.querySelectorAll('a[href]');
+
+    for (const scrollBlock of scrollBlocks) {
+
+      scrollBlock.addEventListener('click', event => {
+        event.preventDefault();
+
+        const id = scrollBlock.getAttribute("href");
+
+        if (id !== '#') {
+          const getId = document.querySelector(id);
+
+          if (getId !== null) {
+            getId.scrollIntoView({
+              behavior: 'smooth',
+              block: 'start',
+            });
+          }
+        }
+      });
+    }
+  };
+
+  scroll();
 };
 
 export default smoothScroll;
